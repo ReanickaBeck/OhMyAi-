4d54bb08d11e4bcc4afdf82960ef3e12df248645
Reanicka Beckford <reanickabeck123@gmail.com>
2024-08-13 10:04:04 +0100

    Initial Commit

---
+++ OhMyAi!/OhMyAi_App.swift
@@ -0,0 +1,17 @@
+//
+//  OhMyAi_App.swift
+//  OhMyAi!
+//
+//  Created by Scholar on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct OhMyAi_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
